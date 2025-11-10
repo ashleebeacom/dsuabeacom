87c1b287275150480b02c79e59c905b514f15732
ashleebeacom <ashlee.beacom@gmail.com>
2025-11-09 20:13:18 -0600

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -5,16 +5,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
-# Example usage
-llm = OpenAI(temperature=0)
-embeddings = OpenAIEmbeddings()
-
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=Chroma(persist_directory="db", embedding_function=embeddings).as_retriever()
-)
-
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
